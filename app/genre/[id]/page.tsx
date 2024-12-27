80b8ceba78eeac9a4f771fed0cf7a093141e394f
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-12-27 13:26:06 -0500

    Adding new way to search for route parameters in next15

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,21 +1,24 @@
 import MoviesCarousel from "@/components/MoviesCarousel";
 import { getDiscoverMovies } from "@/lib/getMovies";
 
-interface PageProps {
-  params: { id: string };
-  searchParams: { genre?: string };
-}
+type Params = Promise<{ id: string }>
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>
 
-export default async function GenrePage({ params, searchParams }: PageProps) {
-  const { id } = params;
-  const genre = searchParams?.genre || "Unknown";
+export default async function GenrePage(props: {
+  params: Params
+  searchParams: SearchParams
+}) {
+  const params = await props.params
+  const searchParams = await props.searchParams
+  const id = params.id
+  const query = searchParams.query
 
   const movies = await getDiscoverMovies(id);
 
   return (
     <div className="max-w-7xl mx-auto mt-20">
       <div className="flex flex-col space-y-4 xl:mt-42">
-        <h1 className="text-4xl font-bold px-5 py-4">Results for {genre}</h1>
+        <h1 className="text-4xl font-bold px-5 py-4">Results for {query}</h1>
       </div>
       <MoviesCarousel title="Genre" movies={movies} isVertical />
     </div>
