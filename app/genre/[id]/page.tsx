cc09a583eb837e7dfff4d81c9cb0a015acb4aad1
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-12-27 12:15:29 -0500

    Fixing eslint error in genre/id/page.tsx

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -2,27 +2,25 @@ import MoviesCarousel from "@/components/MoviesCarousel";
 import { getDiscoverMovies } from "@/lib/getMovies";
 
 type Props = {
-    params: {
-      id: string;
-    };
-    searchParams: {
-        genre: string;
-    };
+  params: {
+    id: string;
   };
+  searchParams: {
+    genre: string;
+  };
+};
 
 async function GenrePage({ params: { id }, searchParams: { genre } }: Props) {
-  
-  const movies = await getDiscoverMovies(id);
-  
+  const movies = await getDiscoverMovies(id, genre);
+
   return (
     <div className="max-w-7xl mx-auto">
-      
       <div className="flex flex-col space-y-4 xl:mt-42">
         <h1 className="text-6xl font-bold px-5">Results for {genre}</h1>
       </div>
-      <MoviesCarousel title="Genre" movies={movies} isVertival />
+      <MoviesCarousel title="Genre" movies={movies} isVertical />
     </div>
-  )
+  );
 }
 
-export default GenrePage
+export default GenrePage;
