a7ac6b69a53c6756955ede4c8ab21796689f6dac
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-12-27 12:52:20 -0500

    Fixing searchParams and stiles in genre page

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -5,18 +5,19 @@ type Props = {
   params: {
     id: string;
   };
-  searchParams: {
-    genre: string;
+  searchParams?: {
+    genre?: string;
   };
 };
 
-async function GenrePage({ params: { id }, searchParams: { genre } }: Props) {
-  const movies = await getDiscoverMovies(id, genre);
+async function GenrePage({ params: { id }, searchParams }: Props) {
+  const movies = await getDiscoverMovies(id);
+  const genre = searchParams?.genre || "Unknown"; 
 
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-7xl mx-auto mt-20">
       <div className="flex flex-col space-y-4 xl:mt-42">
-        <h1 className="text-6xl font-bold px-5">Results for {genre}</h1>
+        <h1 className="text-4xl font-bold px-5 py-4">Results for {genre}</h1>
       </div>
       <MoviesCarousel title="Genre" movies={movies} isVertical />
     </div>
