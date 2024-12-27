35a71b594ac12a470330b9b84406db07700b437c
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-12-27 17:34:51 -0500

    Fixing searchParams.genre in genre page

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -11,14 +11,14 @@ export default async function GenrePage(props: {
   const params = await props.params
   const searchParams = await props.searchParams
   const id = params.id
-  const query = searchParams.query
+  const genre = searchParams.genre
 
   const movies = await getDiscoverMovies(id);
 
   return (
     <div className="max-w-7xl mx-auto mt-20">
       <div className="flex flex-col space-y-4 xl:mt-42">
-        <h1 className="text-4xl font-bold px-5 py-4">Results for {query}</h1>
+        <h1 className="text-4xl font-bold px-5 py-4">Results for {genre}</h1>
       </div>
       <MoviesCarousel title="Genre" movies={movies} isVertical />
     </div>
