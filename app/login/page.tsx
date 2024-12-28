053ddd3837bdf8b10ffe06ef60731fa83169f843
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-12-27 21:02:02 -0500

    Adding loader in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { auth } from "@/firebase"; 
+import { auth } from "@/firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { useRouter } from "next/navigation"; 
+import { useRouter } from "next/navigation";
 
 interface Inputs {
   email: string;
@@ -11,7 +11,8 @@ interface Inputs {
 }
 
 function Login() {
-  const [login, setLogin] = useState(true); 
+  const [login, setLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false); // Estado para el loading
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const router = useRouter();
 
@@ -22,32 +23,42 @@ function Login() {
   } = useForm<Inputs>();
 
   const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    setIsLoading(true); 
     try {
       if (login) {
         await signInWithEmailAndPassword(auth, email, password);
-        router.push("/");
       } else {
         await createUserWithEmailAndPassword(auth, email, password);
-        router.push("/");
       }
+      router.push("/");
     } catch (error: unknown) {
       if (error instanceof Error) {
-        setErrorMessage(error.message); 
+        setErrorMessage(error.message);
       } else {
         setErrorMessage("An unknown error occurred");
       }
+    } finally {
+      setIsLoading(false); 
     }
   };
 
   return (
     <div className="relative flex h-screen w-screen flex-col md:items-center md:justify-center md:bg-transparent">
+       {isLoading && (
+        <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50 transition-opacity opacity-100">
+          <div className="flex flex-col items-center">
+            <p className="mt-4 text-white text-lg font-semibold">Processing...</p>
+          </div>
+        </div>
+      )}
+
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="relative mt-24 space-y-8 rounded py-10 px-6 md:mt-0 md:max-w-md md:px-14"
       >
         <h1 className="text-4xl font-semibold text-white">{login ? "Sign In" : "Sign Up"}</h1>
         {errorMessage && (
-          <div className="text-red-500 text-center mt-4">{errorMessage}</div> // Mostrar mensaje de error
+          <div className="text-red-500 text-center mt-4">{errorMessage}</div>
         )}
         <div className="space-y-4">
           <label className="inline-block w-full">
